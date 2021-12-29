695e2cf9cb6b7ca7cf4e510313e5c931120af95a
Carl Sargunar <carl@sargunar.com>
2021-12-29 23:32:37 +0000

    Repo Commit

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 This demo is for people looking to get started with Docker and Microservices, and is a quick sample I've put together using the open source Umbraco Content Management system, running on Docker on a raspberry Pi. 
 
+
 ## These are the steps to run Umbraco on a Raspberry Pi
 
 Minimum Requirements :
@@ -9,9 +10,6 @@ Minimum Requirements :
 - At least 1 Raspberry Pi 3b+
     - Warning : this WILL be slow on a Pi 3b+
     - A Raspberry Pi 4 4gb or 8gb is recommended
-
-
-
 
 
 ## Documentation
@@ -40,10 +38,14 @@ Minimum Requirements :
 
 ## Links
 
+- Link to Slides : 
+    - [https://docs.google.com/presentation/d/18_ary97bHkR0sygsV-4DuEhQSCGHDJBYGDD33zNO0cQ/](https://docs.google.com/presentation/d/18_ary97bHkR0sygsV-4DuEhQSCGHDJBYGDD33zNO0cQ/)
+
+
 - Umbraco CMS : https://umbraco.com/
     - CMS Source : [https://github.com/umbraco/Umbraco-CMS](https://github.com/umbraco/Umbraco-CMS)
     - Documentation : [https://our.umbraco.com/documentation/reference/cms/](https://our.umbraco.com/documentation/reference/cms/)
 
-- The RGL LED array is based on the Pimoroni Unicorn Hat HD.
+- The RGB LED array is based on the Pimoroni Unicorn Hat HD.
     - You can buy one at [https://shop.pimoroni.com/products/unicorn-hat-hd](https://shop.pimoroni.com/products/unicorn-hat-hd).
     - There's a Git Repository associated with this project [https://github.com/pimoroni/unicorn-hat-hd](https://github.com/pimoroni/unicorn-hat-hd) with a lot of cool samples.
---
+++ UmbDockPi/Controllers/RMQController.cs
@@ -0,0 +1,56 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+using Umbraco.Cms.Web.Common.Controllers;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace UmbDockPi.Controllers
+{
+    public class RMQController : UmbracoApiController
+    {
+        private readonly ILogger<RMQController> _logger;
+        public RMQController(ILogger<RMQController> logger)
+        {
+            _logger = logger;
+        }
+
+        public Microsoft.AspNetCore.Mvc.RedirectResult SendMessage(string name)
+        {
+            _logger.LogInformation("SendMessage : " + name);
+            try
+            {
+                // TODO : Put the hostname in Config
+                // var factory = new ConnectionFactory() { HostName = "192.168.0.144" };
+                //var factory = new ConnectionFactory() { HostName = "localhost" };
+                var factory = new ConnectionFactory() { HostName = "rabbit_mq" };
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    string message = $"Hey {name}! Happy 24 Days of Umbraco. #h5yr!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "hello",
+                                         basicProperties: null,
+                                         body: body);
+                }
+                _logger.LogInformation("SendMessage Completed");
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "SendMessage Error. {0}", ex.Message);
+            }
+
+            // Being Lazy :-)
+            return Redirect("/thank-you/");
+        }
+
+    }
+}
